8178519e9864442b8c8408186812a5d8595f0b0f
Alffen <aliffianghien46@gmail.com>
2021-06-16 21:57:00 +0700

    Aliffian

--- asterix.js
+++ asterix.js
@@ -1,1 +1,22 @@
 // Buatlah asterix dengan kata MAHARDIKA sperti pada contoh di cross.png
+let title = "MAHARDIKA"
+let column = title.length
+let row = title.length
+let result = ""
+
+for (let i = 0; i < title.length; i++) {
+    result = ""
+        for (let j = 0; j < title.length; j++) {
+            if (j == title.length-5) {
+            result += title[i]
+            }
+            else if (i == title.length-5) {
+                result += title[j]
+            }
+            else {
+                // result += "*" memakai * agar terlihat kolomnya
+                result += " "
+            }
+        }
+        console.log(result);
+}
--- calculator.js
+++ calculator.js
@@ -15,7 +15,25 @@
  */
 
 function calculate(num1, num2, operator) {
+
+    let result = ''
     
+    if (operator == '+') {
+        result = num1 + num2
+    }
+    else if (operator == '-') {
+        result = num1 - num2
+    }
+    else if (operator == '*') {
+        result = num1 * num2
+    }
+    else if (operator == '/') {
+        result = num1 / num2
+    }
+    else if (operator == '%') {
+        result = num1 % num2
+    }
+    return result
 }
 
 console.log(calculate(1, 2, '+')) // 3
